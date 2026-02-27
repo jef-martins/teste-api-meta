7b0d8e6841f0234b1ea6d2476c2fba26c2245095
Jef Martins <jef-martins@github>
2026-02-27 13:16:14 -0300

    chore: adiciona handlers SIGINT/SIGTERM comentados para encerramento gracioso

--- index.js
+++ index.js
@@ -14,3 +14,22 @@ bot.iniciar().catch((err) => {
     console.error('[App] Erro fatal ao iniciar o bot:', err);
     process.exit(1);
 });
+
+/*
+let client;
+
+process.on('SIGINT', async () => {
+  console.log('Encerrando bot...');
+  if (client) {
+    await client.close();
+  }
+  process.exit(0);
+});
+
+process.on('SIGTERM', async () => {
+  if (client) {
+    await client.close();
+  }
+  process.exit(0);
+});
+*/
